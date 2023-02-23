9e5af394b8b29a949f9c18388cac2c9cc2df5419
whatshow <wfwfpwefe2323@gmail.com>
2023-02-23 19:13:12 +1100

    update

--- Examples/TestAllFunctions/main.py
+++ Examples/TestAllFunctions/main.py
@@ -1,44 +1,126 @@
 import sys 
 sys.path.append("../..");
 import os
+import pandas
+import numpy as np
+from matplotlib import pyplot as plt
 from RA_Minstrel_SNN import RA_Minstrel_SNN;
 
 # set prefix to store models and 
 model_prefix = "_build_/models_TestAllFunctions/";
 if not os.path.exists(model_prefix):
     os.makedirs(model_prefix);
+# set the train data file
+file_test = "Data/track_yas-wifi-phy_data-beacon.csv";
+# set the train data file
+file_train_prefix = "Data/";
+file_train_list = ["mcs1.csv",  "mcs2.csv",  "mcs3.csv",  "mcs4.csv",  "mcs5.csv",  "mcs6.csv",  "mcs7.csv",  "mcs8.csv",  "mcs9.csv",
+                   "mcs10.csv", "mcs11.csv", "mcs12.csv", "mcs13.csv", "mcs14.csv", "mcs15.csv", "mcs16.csv", "mcs17.csv", "mcs18.csv",
+                   "mcs19.csv", "mcs20.csv", "mcs21.csv", "mcs22.csv", "mcs23.csv", "mcs24.csv", "mcs25.csv", "mcs26.csv", "mcs27.csv", "mcs28.csv"];
+# set the mcs we need
+mcss = [11, 12, 13, 14, 15, 16, 17, 18, 19,
+        20, 21, 22, 23, 24, 25, 26, 27, 28,
+        40, 41, 42, 43, 44, 45, 46, 47, 48, 49];
 
 # init the model
 rms = RA_Minstrel_SNN(snn_model_prefix = model_prefix);
 
-# train 1MHz
-rms.train('Train_Data_New/mcs1.csv', data_file_mcs_assigend = 11);
-rms.train('Train_Data_New/mcs2.csv', data_file_mcs_assigend = 12);
-rms.train('Train_Data_New/mcs3.csv', data_file_mcs_assigend = 13);
-rms.train('Train_Data_New/mcs4.csv', data_file_mcs_assigend = 14);
-rms.train('Train_Data_New/mcs5.csv', data_file_mcs_assigend = 15);
-rms.train('Train_Data_New/mcs6.csv', data_file_mcs_assigend = 16);
-rms.train('Train_Data_New/mcs7.csv', data_file_mcs_assigend = 17);
-rms.train('Train_Data_New/mcs8.csv', data_file_mcs_assigend = 18);
-rms.train('Train_Data_New/mcs9.csv', data_file_mcs_assigend = 19);
-# train 2MHz
-rms.train('Train_Data_New/mcs10.csv', data_file_mcs_assigend = 20);
-rms.train('Train_Data_New/mcs11.csv', data_file_mcs_assigend = 21);
-rms.train('Train_Data_New/mcs12.csv', data_file_mcs_assigend = 22);
-rms.train('Train_Data_New/mcs13.csv', data_file_mcs_assigend = 23);
-rms.train('Train_Data_New/mcs14.csv', data_file_mcs_assigend = 24);
-rms.train('Train_Data_New/mcs15.csv', data_file_mcs_assigend = 25);
-rms.train('Train_Data_New/mcs16.csv', data_file_mcs_assigend = 26);
-rms.train('Train_Data_New/mcs17.csv', data_file_mcs_assigend = 27);
-rms.train('Train_Data_New/mcs18.csv', data_file_mcs_assigend = 28);
-# train 4MHz
-rms.train('Train_Data_New/mcs19.csv', data_file_mcs_assigend = 40);
-rms.train('Train_Data_New/mcs20.csv', data_file_mcs_assigend = 41);
-rms.train('Train_Data_New/mcs21.csv', data_file_mcs_assigend = 42);
-rms.train('Train_Data_New/mcs22.csv', data_file_mcs_assigend = 43);
-rms.train('Train_Data_New/mcs23.csv', data_file_mcs_assigend = 44);
-rms.train('Train_Data_New/mcs24.csv', data_file_mcs_assigend = 45);
-rms.train('Train_Data_New/mcs25.csv', data_file_mcs_assigend = 46);
-rms.train('Train_Data_New/mcs26.csv', data_file_mcs_assigend = 47);
-rms.train('Train_Data_New/mcs27.csv', data_file_mcs_assigend = 48);
-rms.train('Train_Data_New/mcs28.csv', data_file_mcs_assigend = 49);
+# train
+for mcs_index in range(0, len(mcss)):
+    mcs = mcss[mcs_index];
+    file_train = file_train_prefix + file_train_list[mcs_index];
+    if not rms.is_model_loaded(mcs):
+        rms.train(file_train, data_file_mcs_assigend = mcs);
+# have trained
+print("Trained models: %d/%d"%(rms.get_loaded_models_num(), rms.get_supported_models_num()));
+    
+# predict
+test_data_frame = pandas.read_csv(file_test, header=None);
+test_data = test_data_frame.values;
+#test_data = test_data[0:23,:];
+#test_data = test_data[12197:12392,:];
+print("Predicted Data Length = %d"%len(test_data));
+beacon_time = [];
+time = [];
+snr  = [];
+snr_snn = [];
+snr_snn_tmp_bef = 0;
+snr_snn_tmp_cur = 0;
+test_mcs_snn = [];
+test_mcs_snn_bef = 10;
+test_mcs_snn_cur = 10;
+test_mcs_snn_perfect = [];
+test_mcs_minstrel = [];
+
+for i in range(0, len(test_data)):
+    percent = i/len(test_data)*100;
+    print("\r%.2f%%"%percent, end=" ");
+    # base on the packet size to do
+    if test_data[i, 0] != 166:
+        # not data packet
+        # add beacon time
+        beacon_time.append(test_data[i, 2]);
+        # update the current mcs
+        test_mcs_snn_cur = test_mcs_snn_bef;
+        # update the current snr
+        snr_snn_tmp_cur = snr_snn_tmp_bef;
+    else:
+        # data packet
+        # update time
+        time.append(test_data[i, 2]);
+        # update minstrel mcs
+        test_mcs_minstrel.append(test_data[i, -2]);
+        # update SNR
+        snr.append(10*np.log(test_data[i, 3]));
+        # update SNR-SNN
+        test_mcs_snn_bef = 10*np.log(test_data[i, 3]);
+        snr_snn.append(test_mcs_snn_cur);
+        # update SNN mcs
+        mcs = rms.predict(test_data[i, 3]);
+        test_mcs_snn_bef = mcs;
+        test_mcs_snn.append(test_mcs_snn_cur);
+        test_mcs_snn_perfect.append(mcs);
+print('\r100%                                 ');
+
+# draw
+# draw - SNR
+plt.figure(1, figsize=(15, 6), dpi=1200);
+plt.semilogy(time, snr, label='SNR(actual)');
+plt.semilogy(time, snr_snn, label='SNR(SNN)');
+for beacon in beacon_time:
+    plt.axvline(x=beacon, color='y')
+plt.xlabel('time');
+plt.ylabel('snr');
+plt.title("SNR Change");
+plt.legend(loc='best')  
+plt.grid();
+plt.show();
+# draw - MCS
+plt.figure(2, figsize=(15, 6), dpi=1200);
+plt.semilogy(time, test_mcs_minstrel, 'bs-.', label='Minstrel');
+plt.semilogy(time, test_mcs_snn, 'r-', label='SNN');
+plt.semilogy(time, test_mcs_snn_perfect, 'c--', label='SNN (ideal)');
+#plt.xlim([min(time), max(time)]);
+for beacon in beacon_time:
+    plt.axvline(x=beacon, color='y')
+plt.xlabel('time')
+plt.ylabel('MCS')
+plt.title('MCS Change');
+plt.legend(loc='best')  
+plt.grid();
+plt.show();
+# draw - SNR vs MCS perfect
+fig3 = plt.figure(3, figsize=(15, 6), dpi=1200);
+fig3ax1 = fig3.add_subplot(311);
+fig3ax1.semilogy(time, snr, 'r--', label='SNR');
+for beacon in beacon_time:
+    plt.axvline(x=beacon, color='y')
+fig3ax1.set_xlabel('time')
+fig3ax1.set_ylabel('SNR(dB)')
+fig3ax1.set_title('SNN based on SNR');
+fig3ax2 = fig3ax1.twinx();
+fig3ax2.semilogy(time, test_mcs_snn, 'go-.', label='SNN');
+fig3ax2.set_ylabel('MCS')
+plt.legend(loc='best')  
+plt.grid();
+plt.show();
